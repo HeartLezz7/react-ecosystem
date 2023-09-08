4a7f1a85b89bb21d1d16f62dab9f85fbc53b8f6c
HeartLezz7 <heartchantapath@gmail.com>
2023-09-08 13:07:05 +0700

    installanduse-axios

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,32 @@
 import React, { useState,createContext, useContext, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import axios from 'axios';
 
 const AuthContext = createContext();
 
+
 function ContextProvider (props) {
   const [isAuth,setIsAuth] = useState(false);
-  const [isLoading,setIsLoading] = useState(false)
+  const [isLoading,setIsLoading] = useState(false);
+  const [user,setUser] = useState({name: "Guest"});
+  const BASE_URL = "https://jsonplaceholder.typicode.com/users";
 
   // isAuth : false => true ให้ Delay 1 วิ
-  useEffect (()=>{
-    if(isLoading){
-      setTimeout(()=>{
-        setIsLoading(false)
-      },1000)
-    }
-  },[isLoading])
+  // useEffect (()=>{
+  //   if(isLoading){
+  //     setTimeout(()=>{
+  //       setIsLoading(false)
+  //     },1000)
+  //   }
+  // },[isLoading])
 
-  const handleAuth = () => {
-    if(!isAuth) {
-      setIsLoading(true)
-    }
-    setIsAuth(!isAuth)
-  }
+  // const handleAuth = () => {
+  //   if(!isAuth) {
+  //     setIsLoading(true)
+  //   }
+  //   setIsAuth(!isAuth)
+  // }
   // const handleAuth = () => {
   //   if(!isAuth) {
   //     setIsLoading(true)
@@ -34,19 +38,40 @@ function ContextProvider (props) {
   //   setIsAuth(!isAuth)
   // }
 
+  const handleAuth = async () => {
+    if (isAuth){
+      setIsAuth(false);
+      setUser({name: "Guest"});
+      return;
+    }
+      try {
+        setIsLoading(true)
+        const response = await axios.get(`${BASE_URL}/1`);
+        console.log(response.data);
+        setUser(response.data);
+        setIsAuth(true)
+      } catch (error) {
+        console.log(error)
+      }finally{
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1000);
+      }
+    
+  }
 
-  const sharedObj = {isAuth,handleAuth,isLoading}
+  const sharedObj = {isAuth,handleAuth,isLoading,user}
   return <AuthContext.Provider value={sharedObj}> {props.children}</AuthContext.Provider>
 }
 
 function App() {
   // const [isAuth,setIsAuth] = useState(false);
   // const handleAuth = () => {setIsAuth(!isAuth)}
-  const {isAuth,handleAuth,isLoading} = useContext(AuthContext);
+  const {isAuth,handleAuth,isLoading,user} = useContext(AuthContext);
 
   return(
     <div className='App'>
-      {isLoading? <h1>Loading...</h1> : <h1>Welcome : {!isAuth ? "Guest":"User"}</h1>}
+      {isLoading? <h1>Loading...</h1> : <h1>Welcome : {!isAuth ? user?.name:user?.name}</h1>}
    
     <button onClick={handleAuth}>{!isAuth?"login" :"logout"}</button>
   </div>
