51b4ea074bf5b468945206d951ab3cd01d0cdb82
HeartLezz7 <heartchantapath@gmail.com>
2023-09-08 11:28:13 +0700

    finish:use-context

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useState,createContext, useContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+const AuthContext = createContext();
+
+function ContextProvider (props) {
+  const [isAuth,setIsAuth] = useState(false);
+  const handleAuth = () => {setIsAuth(!isAuth)}
+
+
+  const sharedObj = {isAuth,handleAuth}
+  return <AuthContext.Provider value={sharedObj}> {props.children}</AuthContext.Provider>
+}
+
+function App() {
+  // const [isAuth,setIsAuth] = useState(false);
+  // const handleAuth = () => {setIsAuth(!isAuth)}
+  const {isAuth,handleAuth} = useContext(AuthContext);
+
+  return(
+    <div className='App'>
+    <h1>Welcome : {!isAuth ? "Guest":"User"}</h1>
+    <button onClick={handleAuth}>{!isAuth?"login" :"logout"}</button>
+  </div>
+) 
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-<div>Vite React Js</div>)
+  <ContextProvider>
+
+  <App/>
+  </ContextProvider>
+)
