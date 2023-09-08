9c13a057fbdc9b06b232ba5b853bf124c8543e6e
HeartLezz7 <heartchantapath@gmail.com>
2023-09-08 23:44:00 +0700

    mui:grid

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 function App() {
-  return <div>App</div>
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={4}>
+          <Grid item xs={4} sx={{ background: "red" }}>
+            left
+          </Grid>
+          <Grid item xs={4} sx={{ background: "red" }}>
+            center
+          </Grid>
+          <Grid item xs={4} sx={{ background: "red" }}>
+            right
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
 }
-ReactDOM.createRoot(document.getElementById('root')).render(
-<App/>)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
